0824e153e3bec006fecc9da30cd2782476481cc5
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2022-01-19 07:11:54 -0800

    ✅ ADD: Main & Title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <Wrapper>
       <Header />
+      <Main>
+        <MainContent>
+          <TodoContent>
+            <Title>Dashboard</Title>
+            <Greeting>Good morning, David</Greeting>
+          </TodoContent>
+        </MainContent>
+      </Main>
     </Wrapper>
   )
 }
@@ -16,4 +24,31 @@ const Wrapper = styled.div`
   min-height: 100vh;
   min-width: 100vw;
   color: #eee;
+`
+const Main = styled.div`
+  display: flex;
+`
+
+const MainContent = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  transition: 0.3s;
+`
+
+const TodoContent = styled.div`
+  max-width: 700px;
+  width: 100%;
+`
+
+const Title = styled.div`
+  margin: 50px 0;
+  font-size: 30px;
+  font-weight: 700;
+`
+
+const Greeting = styled.div`
+  margin-bottom: 20px;
+  font-size: 36px;
+  font-weight: 800;
 `
